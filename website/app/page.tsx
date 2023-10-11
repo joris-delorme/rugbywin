c4e33afef8f4cbe3ae83a8fbcedb7da6c34dbc69
joris-delorme <joris.delorme@epitech.digital>
2023-10-11 13:23:48 +0200

    website auth

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,62 +1,12 @@
-"use client"
-import mapboxgl, { Map } from 'mapbox-gl'
-import { useEffect, useRef, useState } from 'react';
-import {
-  Card,
-  CardContent
-} from "@/components/ui/card"
-
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9yaXNkZWxvcm1lIiwiYSI6ImNsbXU3MzEwZjBicHgycW1xNG1hN29ldXIifQ.ommqbzSD7Ey152shCR1ZIQ';
-
-
-const MapboxMap = () => {
-  const [map, setMap] = useState<mapboxgl.Map | null>(null);
-  const mapNode = useRef(null);
-
-  function rotateCamera(timestamp) {
-    // clamp the rotation between 0 -360 degrees
-    // Divide timestamp by 100 to slow rotation to ~10 degrees / sec
-    map.rotateTo((timestamp / 100) % 360, { duration: 0 });
-    // Request the next frame of the animation.
-    requestAnimationFrame(rotateCamera);
-  }
-
-  useEffect(() => {
-    const node = mapNode.current
-    if (typeof window === "undefined" || node === null) return;
-
-    const mapboxMap = new mapboxgl.Map({
-      container: node,
-      center: [2.252616, 48.841465],
-      zoom: 16,
-      pitch: 60
-    });
-
-    mapboxMap.on('style.load', () => {
-      //@ts-ignore
-      mapboxMap.setConfigProperty('basemap', 'lightPreset', 'night');
-    })
-
-    setMap(mapboxMap);
-
-    return () => {
-      mapboxMap.remove()
-    }
-  }, [])
-
-  return <div ref={mapNode} className='w-full h-full' />
-}
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function Home() {
-
   return (
-    <main className='flex items-center justify-center h-screen'>
-      <div className='flex items-end w-[350px] h-[600px] relative overflow-hidden rounded-xl'>
-        <div className="w-[350px] h-[600px] top-0 left-0 absolute">
-          <MapboxMap />
-        </div>
-        <div className="h-[200px] z-10 backdrop-blur-xl w-full"></div>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <Button>
+        <Link href='/login'>Connecter vous</Link>
+      </Button>
     </main>
   )
 }
--- website/lib/utils.ts
+++ website/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function firebaseError (err: Error) {
+  if (!err.message) return "Please Try Again"
+  if (err.message === "Firebase: Error (auth/wrong-password).") return "Wrong password."
+  if (err.message === "Firebase: Error (auth/invalid-email).") return "Please enter a valid email."
+  if (err.message === "Firebase: Error (auth/email-already-in-use).") return "Email already in use."
+  return err.message
+}
