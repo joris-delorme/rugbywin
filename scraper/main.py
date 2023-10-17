b7d2f3d9f90cff856314082252923074c9c8ffa7
lelemathrin <leo.mathurin@epitech.digital>
2023-10-17 11:32:54 +0200

    finished old bets

--- scraper/main.py
+++ scraper/main.py
@@ -14,6 +14,8 @@ from mailjet_rest import Client
 import base64
 import json
 import sys
+from datetime import datetime, timedelta
+
 
 print("Script started!")
 
@@ -44,6 +46,18 @@ def run_schedule():
             time.sleep(60)
         time.sleep(10)  # Check every 10 seconds
 
+# Convert american odds to european odds
+
+def american_to_european(american_odds):
+    # Check if the odds are in American format
+    if american_odds.startswith("+"):
+        return round((int(american_odds[1:]) / 100) + 1, 2)
+    elif american_odds.startswith("-"):
+        return round((100 / abs(int(american_odds))) + 1, 2)
+    else:
+        # If not in American format, assume they are already in European format
+        return round(float(american_odds), 2)
+
 # Start the background thread for scheduled tasks
 thread = Thread(target=run_schedule)
 thread.start()
@@ -52,7 +66,112 @@ thread.start()
 atexit.register(lambda: thread.join())
 
 
-# Scraping script
+# Oldbets script beginning
+
+def fetch_oldbets():
+
+    response = requests.get(
+    url='https://proxy.scrapeops.io/v1/',
+    params={
+        'api_key': '26052b57-03c7-499e-abd3-c38571cddb15',
+        'url': 'https://www.oddsportal.com/rugby-union/world/world-cup/results/', 
+        'wait': 5000,
+        'scroll': 3000,
+        'wait': 5000,
+    },
+    )
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    main_div = soup.find("div", attrs={"data-v-012eb9f0": ""})
+    if not main_div:
+        print("Failed to find the main div. The website structure might have changed.")
+        return []
+
+    odds_dict = {}
+    current_date = None
+    event_rows = main_div.find_all("div", class_="eventRow")
+    time_pattern = re.compile(r'^\d{1,2}:\d{2}$')  # A pattern to match formatted times like 14:30
+
+    for event in event_rows:
+        # If the date div exists in this event row, update current_date
+        date_div = event.find(
+            "div",
+            class_="text-black-main font-main w-full truncate text-xs font-normal leading-5",
+        )
+        if date_div:
+            date_text = date_div.text.strip().lower()
+            
+            if "today" in date_text:
+                current_date = datetime.now().strftime('%d %b %Y')
+            elif "yesterday" in date_text:
+                current_date = (datetime.now() - timedelta(days=1)).strftime('%d %b %Y')
+            else:
+                # Extract the date using regular expression
+                match = re.search(r"\d{2} \w{3} \d{4}", date_text)
+                if match:
+                    current_date = match.group(0)
+
+        # Extracting match time
+        match_time = "Unknown"
+        for p in event.find_all("p"):
+            if time_pattern.match(p.text.strip()):
+                match_time = p.text.strip()
+                break
+
+        # Extracting the two teams and their scores from the event
+        teams = [
+            a["title"] for a in event.find_all("a", title=True) if "title" in a.attrs
+        ]
+        scores = []
+        for t in event.find_all("a", title=True):
+            all_div = t.find_all("div")
+            scores.append(all_div[1].text.strip())
+
+        score1, score2 = "N/A", "N/A"
+        if len(scores) == 2:
+            score1, score2 = scores
+
+        if len(teams) < 2:
+            print("Warning: Less than two teams found for an event. Skipping...")
+            continue
+
+        team1, team2 = teams
+
+        # Extracting the odds for each team
+        odds = event.find_all("p", class_="height-content", limit=3)
+
+        if len(odds) < 3:
+            print("Warning: Less than 3 odds found for an event. Skipping...")
+            continue
+
+        # Extracting the odds for each team and convert if necessary
+        odds = event.find_all("p", class_="height-content", limit=3)
+        odd_team1 = str(american_to_european(odds[0].text.strip()))
+        oddDraw = str(american_to_european(odds[1].text.strip()))
+        odd_team2 = str(american_to_european(odds[2].text.strip()))
+
+        # Store odds with teams as keys
+        odds_dict[(team1, team2)] = {
+            "team_a": team1,
+            "team_b": team2,
+            "score_a": score1,
+            "score_b": score2,
+            "team_a_odds": odd_team1,
+            "oddDraw": oddDraw,
+            "team_b_odds": odd_team2,
+            "time": match_time,
+            "date": current_date,
+        }
+
+    return odds_dict
+
+# Oldbets script finish
+
+
+# Rugby World Cup and Unibet beginning
+
 def fetch_rugby_data():
     try:
         global scrape_count, scraped_data
@@ -222,29 +341,42 @@ def fetch_rugby_data():
                 # Update the dictionary
                 outcome_bets_dict[(team1, team2)] = (bet1, bet2)
 
-        # Step 2: Assigning bets to matches in all_matches_info
+        driver.quit()
+        # End of the unibet script
 
+        odds = fetch_oldbets()  # Fetch odds data
+
+        # Assigning bets to matches in all_matches_info
         for match_info in all_matches_info:
             team_a = match_info["teams"]["team_a"]
             team_b = match_info["teams"]["team_b"]
-            
+
+            # Check if the match has finished
             if match_info["status"].lower() == "result":
-                match_info["teams"]["bet_a"] = "Closed"
-                match_info["teams"]["bet_b"] = "Closed"
+                # Get the odds for the specific finished match from the old bets data
+                odds_for_match = odds.get((team_a, team_b), None)
+
+                # If there's odds data for the finished match, assign it
+                if odds_for_match:
+                    match_info["teams"]["bet_a"] = odds_for_match.get('team_a_odds', "N/A")
+                    match_info["teams"]["bet_b"] = odds_for_match.get('team_b_odds', "N/A")
+                else:
+                    # If there's no old odds data for the finished match, mark it as closed
+                    match_info["teams"]["bet_a"] = "Closed"
+                    match_info["teams"]["bet_b"] = "Closed"
+
+            # For upcoming matches, fetch the odds data from the outcome_bets_dict
             else:
-                # Get the bets using the team names
-                bet_a, bet_b = outcome_bets_dict.get((team_a, team_b), ("N/A", "N/A"))
-                match_info["teams"]["bet_a"] = bet_a
-                match_info["teams"]["bet_b"] = bet_b
+                upcoming_odds = outcome_bets_dict.get((team_a, team_b), (None, None))
+                match_info["teams"]["bet_a"] = upcoming_odds[0] if upcoming_odds[0] else "N/A"
+                match_info["teams"]["bet_b"] = upcoming_odds[1] if upcoming_odds[1] else "N/A"
 
-        driver.quit()
-        # End of the scraping script
-
-
+        # After iterating through all matches, we'll have updated the old odds for finished matches
         scraped_data = {
-        'status': 'success',
-        'data': all_matches_info
+            'status': 'success',
+            'data': all_matches_info
         }
+
         scrape_count += 1
         print("The scrape count is : ", scrape_count, "/500")
 
@@ -257,6 +389,8 @@ def fetch_rugby_data():
             'message': f"Failed to fetch data. Reason: {e}"
         }
 
+# Rugby World Cup and Unibet beginning
+
 def send_email_with_json(data):
     try: 
         # Get your Mailjet API and Secret Key (either from environment variables or securely)
@@ -267,7 +401,7 @@ def send_email_with_json(data):
         
         # Create the JSON file
         with open("rugby_data.json", "w") as file:
-            json.dump(data, file)
+            json.dump(data, file, indent=4)
 
         # Email details
         email_data = {
@@ -281,10 +415,14 @@ def send_email_with_json(data):
                         {
                             "Email": "mathurinleo69@gmail.com",
                             "Name": "Leo"
+                        },
+                        {
+                            "Email": "hello@jorisdelorme.fr",
+                            "Name": "Joris"
                         }
                     ],
-                    "Subject": "Rugby Data JSON",
-                    "TextPart": "Here's the JSON data you requested.",
+                    "Subject": "Rugby Data Update",
+                    "TextPart": "There's a new update on the RugbyWin Data",
                     "Attachments": [
                         {
                             "ContentType": "application/json",
@@ -321,7 +459,11 @@ def get_rugby_data():
 
 if __name__ == '__main__':
     data = fetch_rugby_data()
-    send_email_with_json(data)
+    # Save data to JSON file
+    with open("data.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+    print("Data saved to 'data.json'.")
 
 print("Finished script")
 
--- scraper/sites/oldbets.py
+++ scraper/sites/oldbets.py
@@ -1,44 +1,123 @@
-from flask import Flask, request, jsonify, make_response
-from threading import Thread
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
+import requests
+import json
 import re
+from datetime import datetime, timedelta
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
 
-driver = webdriver.Chrome(options=chrome_options)
-
-driver.get('https://www.oddsportal.com/rugby-union/world/world-cup/results/')
-
-# Get page source and parse it with BeautifulSoup
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-
-# Initialize list to store results
-matches = []
-
-# Loop through each div with class 'eventRow'
-for event in soup.find_all('div', class_='eventRow'):
-    a = soup.find('a')
-    match = {}
+def american_to_european(american_odds):
+    # Check if the odds are in American format
+    if american_odds.startswith("+"):
+        return round((int(american_odds[1:]) / 100) + 1, 2)
+    elif american_odds.startswith("-"):
+        return round((100 / abs(int(american_odds))) + 1, 2)
+    else:
+        # If not in American format, assume they are already in European format
+        return round(float(american_odds), 2)
     
-    # Get teams from anchor tags
-    teams = a.select('a')[:2]   # Gets the first 2 anchor tags inside the div
-    match['team1'] = teams[0].get_text(strip=True)
-    match['team2'] = teams[1].get_text(strip=True)
-    
-    # Get odds from paragraph tags
-    odds = a.select('p')[:3:2]  # Gets the 1st and 3rd paragraph tags inside the div
-    match['odd_team1'] = odds[0].get_text(strip=True)
-    match['odd_team2'] = odds[1].get_text(strip=True)
-    
-    matches.append(match)
 
-# Print the results
-for match in matches:
-    print(f"{match['team1']} ({match['odd_team1']}) - {match['team2']} ({match['odd_team2']})")
+def fetch_oldbets():
+
+    response = requests.get(
+    url='https://proxy.scrapeops.io/v1/',
+    params={
+        'api_key': '26052b57-03c7-499e-abd3-c38571cddb15',
+        'url': 'https://www.oddsportal.com/rugby-union/world/world-cup/results/', 
+        'wait': 5000,
+        'scroll': 3000,
+        'wait': 5000,
+    },
+    )
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    main_div = soup.find("div", attrs={"data-v-012eb9f0": ""})
+    if not main_div:
+        print("Failed to find the main div. The website structure might have changed.")
+        return []
+
+    matches = []
+    current_date = None
+    event_rows = main_div.find_all("div", class_="eventRow")
+    time_pattern = re.compile(r'^\d{1,2}:\d{2}$')  # A pattern to match formatted times like 14:30
+
+    for event in event_rows:
+        # If the date div exists in this event row, update current_date
+        date_div = event.find(
+            "div",
+            class_="text-black-main font-main w-full truncate text-xs font-normal leading-5",
+        )
+        if date_div:
+            date_text = date_div.text.strip().lower()
+            
+            if "today" in date_text:
+                current_date = datetime.now().strftime('%d %b %Y')
+            elif "yesterday" in date_text:
+                current_date = (datetime.now() - timedelta(days=1)).strftime('%d %b %Y')
+            else:
+                # Extract the date using regular expression
+                match = re.search(r"\d{2} \w{3} \d{4}", date_text)
+                if match:
+                    current_date = match.group(0)
+
+        # Extracting match time
+        match_time = "Unknown"
+        for p in event.find_all("p"):
+            if time_pattern.match(p.text.strip()):
+                match_time = p.text.strip()
+                break
+
+        # Extracting the two teams and their scores from the event
+        teams = [
+            a["title"] for a in event.find_all("a", title=True) if "title" in a.attrs
+        ]
+        scores = []
+        for t in event.find_all("a", title=True):
+            all_div = t.find_all("div")
+            scores.append(all_div[1].text.strip())
+
+        score1, score2 = "N/A", "N/A"
+        if len(scores) == 2:
+            score1, score2 = scores
+
+        if len(teams) < 2:
+            print("Warning: Less than two teams found for an event. Skipping...")
+            continue
+
+        team1, team2 = teams
+
+        # Extracting the odds for each team
+        odds = event.find_all("p", class_="height-content", limit=3)
+
+        if len(odds) < 3:
+            print("Warning: Less than 3 odds found for an event. Skipping...")
+            continue
+
+        # Extracting the odds for each team and convert if necessary
+        odds = event.find_all("p", class_="height-content", limit=3)
+        odd_team1 = str(american_to_european(odds[0].text.strip()))
+        oddDraw = str(american_to_european(odds[1].text.strip()))
+        odd_team2 = str(american_to_european(odds[2].text.strip()))
+
+        matches.append({
+            "team1": team1,
+            "team2": team2,
+            "score1": score1,
+            "score2": score2,
+            "odd_team1": odd_team1,
+            "oddDraw": oddDraw,
+            "odd_team2": odd_team2,
+            "time": match_time,
+            "date": current_date,  # Use the current_date for this match
+        })
+
+    return matches
+
+data = fetch_oldbets()
+
+# Save data to JSON file
+with open("world_cup_data.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+print("Data saved to 'world_cup_data.json'.")
